04ee500429b2230c3f06fc845ffe1f57d0440df9
miremi9 <56270462+miremi9@users.noreply.github.com>
2023-06-03 16:41:35 +0200

    tente de reparer le bazar

--- CentralJavaProject/src/serial/ApplicationCentrale.java
+++ CentralJavaProject/src/serial/ApplicationCentrale.java
@@ -1,17 +1,88 @@
 package serial;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
-public interface ApplicationCentrale {
-	ArrayList<Employee> getEmployee();
-	void addEmployee(Employee param);
-	void deleteEmployee(Employee param);
+public class ApplicationCentrale {
+	ArrayList<Employee> ListEmployee;
+	ArrayList<Timeclock> ListTimeclock;
+	int Nbemployee;
+	int Nbtimeclock;
+
+	public ApplicationCentrale() {
+		Nbemployee = 0;
+		Nbtimeclock = 0;
+		ListEmployee = new ArrayList<Employee>();
+		ListTimeclock = new ArrayList<Timeclock>();
+	}
+
 	
-	void addPointage(Timeclock param);
-	Timeclock[] getTimeclock();
+	public ArrayList<Employee> getEmployee() {
+ 		return ListEmployee;
+
+	}
+
 	
-	float calculateTimeCredit(Employee param);
+	public void addEmployee(Employee param) {
+		ListEmployee.add(param);
+		Nbtimeclock++;
+
+	}
+
 	
-	void saveData();
-	void getData();
+	public void deleteEmployee(Employee param) {
+		ListEmployee.remove(param);
+	}
+
+	
+	public void addPointage(Timeclock param) {
+		
+	}
+
+	
+	public Timeclock[] getTimeclock() {
+		return null;
+	}
+
+	
+	public float calculateTimeCredit(Employee param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	public void saveData() {
+		try (FileOutputStream fos = new FileOutputStream("listData");
+				ObjectOutputStream oos = new ObjectOutputStream(fos);) {
+
+			oos.writeObject(ListEmployee);
+
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public void getData() {
+		try (FileInputStream fis = new FileInputStream("listData");
+		    ObjectInputStream ois = new ObjectInputStream(fis);) {
+
+		  ListEmployee = (ArrayList<Employee>) ois.readObject();
+		} catch (IOException ioe) {
+		  ioe.printStackTrace();
+		} catch (ClassNotFoundException c) {
+		  System.out.println("Class not found");
+		  c.printStackTrace();
+		}
+
+	}
+
 }
